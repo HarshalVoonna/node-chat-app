00e5229ea66d99f6e59aa1ff401acdb3de4e9bc2
Shreeharsha Nageshwar Voonna <shreeharsha.nageshwarvoonna@citrix.com>
2017-10-21 19:06:40 +0530

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -25,10 +25,12 @@ socket.on('disconnect', function() {
 
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
+  var messageTextbox = $('[name=message]');
   socket.emit('createMessage', {
     from: 'Harsha',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function(data) {
+    messageTextbox.val('');
   });
 });
 
@@ -37,13 +39,16 @@ locationButton.on('click', function() {
   if (!navigator.geolocation){
     return alert('geolocation not supported by your browser');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function (position) {
     console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
-    })
+    });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
