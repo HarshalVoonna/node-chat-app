d16c6865360f2e079f96d493e45b05ed226fe500
Shreeharsha Nageshwar Voonna <shreeharsha.nageshwarvoonna@citrix.com>
2017-10-23 22:18:16 +0530

    Set Room field based on drop down menu

--- public/js/index.js
+++ public/js/index.js
@@ -9,11 +9,20 @@ socket.on('setRoomList', function(roomList) {
   console.log(roomList);
   if(roomList.length > 0){
     var availablerooms = $('#available-rooms');
-    var select = $('<select></select>');
+    var select = $('<select onchange="selectRoomOption(this)"></select>');
     roomList.forEach(function(room) {
+      select.append($('<option></option>').attr('value','Select a room').text('Select a room'));
       select.append($('<option></option>').attr('value',room).text(room));
     });
     availablerooms.append($('<label></label>').text('Available Rooms'));
     availablerooms.append(select);
   }
 });
+
+function selectRoomOption(room) {
+  if(room.value === 'Select a room'){
+    $('#roomfield').val('');
+  } else {
+    $('#roomfield').val(room.value);
+  }
+}
